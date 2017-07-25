4b4b41725775d1381e431acd0fd0bc35ab110125
zhallalxj <zhallalxj5201314>
2017-07-25 17:48:33 +0800

    添加swagger-ui

--- test-web/src/main/java/org/zh/controller/TestController.java
+++ test-web/src/main/java/org/zh/controller/TestController.java
@@ -1,18 +1,23 @@
 package org.zh.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * 测试Controller
  * Created by ZhaoHang on 2017/7/25.
  */
 @Controller
+@Api(value = "测试", description = "测试")
 @RequestMapping(value = "/test")
 public class TestController {
 
-    @RequestMapping
+    @RequestMapping(method = RequestMethod.GET)
+    @ApiOperation(value = "测试接口")
     public String test(Model model) {
         return "views/index";
     }
